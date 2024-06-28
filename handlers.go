d98bff84b9cbef2f992c9ae9b4c1703784d8ae73
massilb <massil.braik@ynov.com>
2024-06-28 12:55:30 +0200

    creation of the home and login empty templates and handlers funcs

---
+++ handlers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+)
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	t, err := template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, nil)
+
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home")
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w , "login")
+}
+
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+const port = ":8080"
 
 func main() {
-	fmt.Println("Test")
+	// Servir les fichiers statiques du répertoire "static"
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/login" , Login)
+	fmt.Println("server started on port" + port)
+	fmt.Println("http://localhost" + port)
+	http.ListenAndServe(port, nil)
 }
