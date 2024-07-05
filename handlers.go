378845e4636c4a525ff700e9da0a627500a6f867
massilb <massil.braik@ynov.com>
2024-07-05 11:26:19 +0200

    login and register func separated from the handler funcs

--- handlers.go
+++ handlers.go
@@ -13,14 +13,7 @@ import (
 
 var sessions = map[string]int{}
 
-func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	t, err := template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	t.Execute(w, data)
-}
+
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	user := getUserFromSession(r)
@@ -28,32 +21,108 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		username := r.FormValue("loginUsername")
-		password := r.FormValue("loginPassword")
-
-		exist, ID := src.CheckIfUserExist(username, username)
-		if !exist {
-			fmt.Println("Incorrect Username or password")
-		}
-		User := src.GetUserByID(ID)
-
-		match := User.CheckPassword(password)
-
-		if match {
-			fmt.Println("Connected as ", User.GetUsername())
-			setUserSession(w, &User)
-			http.Redirect(w, r, "/", http.StatusSeeOther)
-			return
-		} else {
-			fmt.Println("Incorrect Username or password")
-		}
-	}
+	login(w,r)
 	user := getUserFromSession(r)
 	renderTemplate(w, "login", user)
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
+	register(w, r)
+	renderTemplate(w, "register", nil)
+}
+
+func Categories(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromSession(r)
+	renderTemplate(w, "categories", user)
+}
+
+func Profile(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromSession(r)
+	renderTemplate(w, "profile", user)
+}
+
+func CreateCategory(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromSession(r)
+	renderTemplate(w, "create-category", user)
+}
+
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromSession(r)
+	renderTemplate(w, "create-post", user)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func getUserFromSession(r *http.Request) *class.User {
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		return nil
+	}
+	userID, ok := sessions[cookie.Value]
+	if !ok {
+		return nil
+	}
+	user := src.GetUserByID(userID)
+	return &user
+}
+
+
+
+func setUserSession(w http.ResponseWriter, user *class.User) {
+	// Generate a UUID for the session token
+	sessionToken, err := uuid.NewV4()
+	if err != nil {
+		http.Error(w, "Error generating session token", http.StatusInternalServerError)
+		return
+	}
+	sessions[sessionToken.String()] = user.GetID()
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   sessionToken.String(),
+		Expires: time.Now().Add(10 * time.Minute),
+	})
+}
+
+
+
+
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	// Supprimer la session
+	delete(sessions, cookie.Value)
+
+	// Expirer le cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   "",
+		Expires: time.Now().Add(-1 * time.Hour),
+	})
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
+
+
+func register(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		username := r.FormValue("username")
 		email := r.FormValue("email")
@@ -83,74 +152,39 @@ func Register(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("password and confirmpassword are not the same")
 		}
 	}
-	renderTemplate(w, "register", nil)
 }
 
-func Categories(w http.ResponseWriter, r *http.Request) {
-	user := getUserFromSession(r)
-	renderTemplate(w, "categories", user)
-}
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		username := r.FormValue("loginUsername")
+		password := r.FormValue("loginPassword")
 
-func Profile(w http.ResponseWriter, r *http.Request) {
-	user := getUserFromSession(r)
-	renderTemplate(w, "profile", user)
-}
+		exist, ID := src.CheckIfUserExist(username, username)
+		if !exist {
+			fmt.Println("Incorrect Username or password")
+		}
+		User := src.GetUserByID(ID)
 
-func CreateCategory(w http.ResponseWriter, r *http.Request) {
-	user := getUserFromSession(r)
-	renderTemplate(w, "create-category", user)
-}
+		match := User.CheckPassword(password)
 
-func CreatePost(w http.ResponseWriter, r *http.Request) {
-	user := getUserFromSession(r)
-	renderTemplate(w, "create-post", user)
-}
-
-func getUserFromSession(r *http.Request) *class.User {
-	cookie, err := r.Cookie("session_token")
-	if err != nil {
-		return nil
+		if match {
+			fmt.Println("Connected as ", User.GetUsername())
+			setUserSession(w, &User)
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		} else {
+			fmt.Println("Incorrect Username or password")
+		}
 	}
-	userID, ok := sessions[cookie.Value]
-	if !ok {
-		return nil
-	}
-	user := src.GetUserByID(userID)
-	return &user
 }
 
-func setUserSession(w http.ResponseWriter, user *class.User) {
-	// Generate a UUID for the session token
-	sessionToken, err := uuid.NewV4()
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	t, err := template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
 	if err != nil {
-		http.Error(w, "Error generating session token", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	sessions[sessionToken.String()] = user.GetID()
-	http.SetCookie(w, &http.Cookie{
-		Name:    "session_token",
-		Value:   sessionToken.String(),
-		Expires: time.Now().Add(10 * time.Minute),
-	})
-}
-
-func Logout(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("session_token")
-	if err != nil {
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
-		return
-	}
-
-	// Supprimer la session
-	delete(sessions, cookie.Value)
-
-	// Expirer le cookie
-	http.SetCookie(w, &http.Cookie{
-		Name:    "session_token",
-		Value:   "",
-		Expires: time.Now().Add(-1 * time.Hour),
-	})
-
-	http.Redirect(w, r, "/login", http.StatusSeeOther)
+	t.Execute(w, data)
 }
 
